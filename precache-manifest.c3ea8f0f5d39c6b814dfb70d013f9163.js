93571501791d7386224ac950b885b9ae30313f26
scrazen <scrazen@gmail.com>
2019-09-26 13:44:33 -0500

    Updates

---
+++ precache-manifest.c3ea8f0f5d39c6b814dfb70d013f9163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02dba115b597178535e8566f1ce2d6a4",
+    "url": "/incremental-game/index.html"
+  },
+  {
+    "revision": "697d99bb0cb655c89384",
+    "url": "/incremental-game/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "db87d796af50937ffcc3",
+    "url": "/incremental-game/static/js/2.16919252.chunk.js"
+  },
+  {
+    "revision": "697d99bb0cb655c89384",
+    "url": "/incremental-game/static/js/main.fee05051.chunk.js"
+  },
+  {
+    "revision": "4f2be9a7e775ca9f714d",
+    "url": "/incremental-game/static/js/runtime-main.d0d415c4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/incremental-game/static/media/logo.25bf045c.svg"
+  }
+]);
